e3c3cd2a0da3d39018f52d7897af7e9bad382665
Kiran <kirankumar.meadiga@gmail.com>
2019-11-29 10:25:39 +0530

    Android Files

---
+++ app/src/main/java/module143/com/myapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package module143.com.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
